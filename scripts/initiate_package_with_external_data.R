17734cd8a1344897e74a962d7b22c697eab3d1d9
Christian Roy <christian.roy@affinittx.com>
2025-03-24 17:31:33 +0000

    More remote troubleshooting

--- scripts/initiate_package_with_external_data.R
+++ scripts/initiate_package_with_external_data.R
@@ -23,7 +23,11 @@ if (length(missing_files) > 0) {
   source("data-raw/nmdp_hla_frequencies_us_2020_census_adjusted.R")
   source("data-raw/census_adjusted_nmdp_hla_frequencies_by_state.R")
   source("data-raw/census_adjusted_nmdp_hla_frequencies_by_county.R")
-  source("data-raw/add_catchment_calculations_and_data.R")
-  
+  source("data-raw/add_catchment_calculations_and_data.R") 
+
   cat("All scripts have been successfully run.\n")
+  # Now print that we're doing to document objects and functions in the package
+  cat("Now documenting objects and functions in the package.\n")
+  devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
+  cat("Documentation has been successfully generated.\n")
 }
